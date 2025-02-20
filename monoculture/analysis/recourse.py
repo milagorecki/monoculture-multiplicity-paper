ce98949d493b834cee94d0f4186dcf863661f39b
mgorecki <mila.gorecki@tuebingen.mpg.de>
2025-02-20 18:45:44 +0100

    restrict poisson_binomial to M<= 30

--- monoculture/analysis/recourse.py
+++ monoculture/analysis/recourse.py
@@ -3,47 +3,69 @@
 from typing import List, Union
 import pandas as pd
 import torch
+import numpy as np
 
 import more_itertools
 from functools import reduce
 from operator import mul, add
+import logging
 
 
-def baseline_agreement(
+def poisson_binom_agreement(
     fn_rates: Union[List[float], torch.Tensor],
     k: int,
     epsilon: float = 0.0001,
 ):
     """
-    Poisson Binomal Model: Assume each model to correspond to an independent
-    Bernoulli trial with the sucess probability given by the TPR (1-FNR).
-    What is the probability of k models to reject?
+    Poisson Binomal Model: Assume each model to correspond to an independent Bernoulli trial with the sucess probability given by the TPR (1-FNR). What is the probability of k models to reject?
     """
     M = len(fn_rates)
-    # make sure none of the rates is exactly 0 or 1, otherwise one term will set whole product to 0 --- test line length2
-    fn_rates = [r + (epsilon * (r == 0)) - (epsilon * (r == 1)) for r in fn_rates]
+    # make sure none of the rates is exactly 0 or 1, otherwise one term will set whole product to 0 --- test line length
+    fn_rates = torch.Tensor(
+        [r + (epsilon * (r == 0)) - (epsilon * (r == 1)) for r in fn_rates]
+    )
 
-    # creatd initial list to indicate which model rejects
-    rej_indicators = torch.concat(
-        [torch.ones(k, dtype=int), torch.zeros(M - k, dtype=int)]
-    ).tolist()
-    # get all unique permutations of k rejections
-    unique_permutations = more_itertools.distinct_permutations(rej_indicators)
+    if M <= 30:
+        logging.info("Computing exact expectation using closed form.")
+        # created initial list to indicate which model rejects
+        rej_indicators = torch.concat(
+            [torch.ones(k, dtype=bool), torch.zeros(M - k, dtype=bool)]
+        ).tolist()
+        # get all unique permutations of k rejections
+        logging.info(f"Getting distinct permutations for {M} models and {k} rejects")
+        unique_permutations = list(more_itertools.distinct_permutations(rej_indicators))
 
-    products = []
-    for indices in unique_permutations:
-        # for each permutations, compute the product of fnrs
-        products.append(
-            reduce(
-                mul,
-                [
-                    p ** indices[i] * (1 - p) ** (1 - indices[i])
-                    for i, p in enumerate(fn_rates)
-                ],
-            )
+        logging.debug(
+            f"Compute probabilities under Poisson binomal model for {len(unique_permutations)} distinct permutations"
         )
-    # sum over all permutations
-    return reduce(add, products)
+        permutation_mat = torch.Tensor(unique_permutations).bool()
+        fn_rates_permut = torch.matmul(permutation_mat.float(), torch.diag(fn_rates))
+        tp_rates_permut = torch.matmul(
+            (~permutation_mat).float(), torch.diag(1 - fn_rates)
+        )
+
+        # products = []
+        # for indices in unique_permutations:
+        #     # for each permutations, compute the product of fnrs
+        #     fnr_or_tpr_based_on_permutation = [
+        #         p ** indices[i] * (1 - p) ** (1 - indices[i])
+        #         for i, p in enumerate(fn_rates)
+        #     ]
+        #     print(indices, fnr_or_tpr_based_on_permutation)
+        #     products.append(
+        #         reduce(
+        #             mul,
+        #             fnr_or_tpr_based_on_permutation,
+        #         )
+        #     )
+        # sum over all permutations
+        # return reduce(add, products)
+        return torch.prod(tp_rates_permut + fn_rates_permut, axis=1).sum(axis=0).item()
+    else:
+        logging.info(
+            f"M too large, Estimate expectation by sampling fixed number of permutations."
+        )
+        raise NotImplementedError("Simulated expectation is not yet implemented")
 
 
 def expected_agreement_two_models(acc1: float, acc2: float) -> float:
